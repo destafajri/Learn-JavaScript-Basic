e03c9c50f71e27cb260e3e16b07f3a613ff0d715
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-10-04 19:42:10 +0700

    init

--- 10-OOP/main.js
+++ 10-OOP/main.js
@@ -1,10 +1,19 @@
 //constructor function
 function Person(firstName, lastName, dob){
+    //setter
     this.firstName = firstName
     this.lastName = lastName
     this.dob = new Date(dob)
+    //getter
+    this.getFullname = function(){
+        return `${this.firstName} ${this.lastName}`
+    }
+    this.getBirthDayYear = function(){
+        return this.dob.getFullYear()
+    }
 }
 
 //instantiate object
 let person1 = new Person('John', 'Mayer', '03-03-1990')
-console.log(person1.dob)
+console.log(person1.getBirthDayYear())
+console.log(person1.getFullname())
