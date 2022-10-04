f5d5fea3fbfdc05a9f0b39ccc7b1522af2f78b6d
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-10-04 17:44:29 +0700

    init

---
+++ 02-Var, let, const/main.js
@@ -0,0 +1,11 @@
+// var, let, const
+
+//dinamic variable, can reassign
+let age = 30
+console.log(age)
+age = 22
+console.log(age)
+
+//not dinamic variable, can't reassign
+const price = 20000
+console.log(price)
